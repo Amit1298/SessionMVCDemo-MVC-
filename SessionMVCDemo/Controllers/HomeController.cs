4c62f8a0d7161032bdbaf7b9d9afe8e2c7e73991
Amit <amitkr24031995@gmail.com>
2022-03-01 22:35:50 +0530

    Adding SessionMVCDemo

---
+++ SessionMVCDemo/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SessionMVCDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewData["Var1"] = "Data comes from ViewData";
+            ViewBag.Var2 = "Data comes from ViewBag";
+            TempData["Var3"] = "Data comes from TempData";
+            Session["Var4"] = "Data comes from Session";
+
+            string[] student = { "Amit", "Krishna", "Lala", "Satty", "Vimal" };
+            Session["Var5"] = student;
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
